6102f93609c1644f1352ae0bbc48975b6ad65f11
caldeira04 <jp.caldeira04@gmail.com>
2023-08-27 17:10:21 -0300

    Created final message/defined vars for functions

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import datetime
 locale.setlocale(locale.LC_TIME, 'pt_BR.utf8')
 
 #Starts a system to get the greetings message
+
 def initMessage():
     now = datetime.datetime.now()
     currentHour = now.hour
@@ -20,25 +21,32 @@ def initMessage():
     else:
         return f"Boa noite pessoal!\n\n" #Good evening everyone!
 
+#Defines the middle message, that goes between the sportsMessages and sportsList
+
 def middleMessage():
     now = datetime.datetime.now()
     currentHour = now.hour
 
     if currentHour < 12:
-        return f"Horários nobres disponíveis para HOJE - ({now.strftime('%d/%m')}, {now.strftime('*%A*')}):"
+        return f"Horários nobres vagos para HOJE - ({now.strftime('%d/%m')}, {now.strftime('*%A*')}):\n\n"
 
     elif currentHour < 18:
-        return f"Últimos horários nobres vagos para HOJE - ({now.strftime('%d/%m')}, {now.strftime('*%A*')}):"
+        return f"Últimos horários nobres vagos para HOJE - ({now.strftime('%d/%m')}, {now.strftime('*%A*')}):\n\n"
 
     else:
         tomorrow = now + datetime.timedelta(days=1)
-        return f"Boa noite pessoal!\n\n{motd}\n\nHorários nobres vagos para AMANHÃ ({tomorrow.strftime('%d/%m')}, {tomorrow.strftime('*%A*')}):"
+        return f"Horários nobres vagos para AMANHÃ ({tomorrow.strftime('%d/%m')}, {tomorrow.strftime('*%A*')})\n\n:"
 
-
+final = "\n\nConsulte disponibilidade para os demais horários"
+greetings = initMessage()
+middle = middleMessage()
 sportsMessages = (padel.actualMessage, beach.actualMessage) 
 sportsList = (padel.times, beach.times)
 sportsPromo = (padel.promo, beach.promo)
 
+print(greetings)
 print(sportsMessages)
+print(middle)
 print(sportsList)
 print(sportsPromo)
+print(final)
